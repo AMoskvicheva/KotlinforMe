92881879de233680fb4e89dfff12a7531b5b0aaf
AMoskvicheva <149317632+AMoskvicheva@users.noreply.github.com>
2023-12-13 21:49:04 +0600

    Update 1.kt
    Добавление конструктора

--- 1.kt
+++ 1.kt
@@ -4,27 +4,21 @@ package com.example.oo
  * 1. Создайте класс Car с полями model, color, year и методом drive().Создайте несколько объектов
  * этого класса и вызовите их метод drive().
  */
-open class Car {
-    var model = "Niva"
-    var color = "White"
-    var year = "2010"
+open class Car(model: String, color: String, year: String) {
+    var m = model
+    var c = color
+    var y = year
     fun drive() {
-        println("$model $color $year поехала")
+        println("Поехала машина $m $c $y")
     }
 }
 
 fun main() {
-    val myCar1 = Car() // экземпляры класса мы всегда добавляем вне класса?
+    val myCar1 = Car(model = "Priora", color = "violet", year = "2000")
+    val myCar2 = Car(model = "Niva", color = "white", year = "1987")
+    val myCar3 = Car(model = "Volkswagen", color = "red", year = "2023")
     myCar1.drive()
-    val myCar2 = Car()
-    myCar2.model = "Priora"
-    myCar2.color = "Violet"
-    myCar2.year = "2000"
     myCar2.drive()
-    val myCar3 = Car()
-    myCar3.model = "Volga"
-    myCar3.color = "Black"
-    myCar3.year = "1970"
     myCar3.drive()
 }
 
