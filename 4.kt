afc06f076f22480a5d3e823143f315f55c33b2cb
AMoskvicheva <149317632+AMoskvicheva@users.noreply.github.com>
2023-12-14 14:22:36 +0600

    Update 4.kt
    добавлен конструктор

--- 4.kt
+++ 4.kt
@@ -3,16 +3,8 @@ package com.example.oo
 /**
  * 4. Создайте класс Person с приватными свойствами name, age и публичными методами getName()
  * и getAge(). Проверьте, что прямой доступ к этим свойствам невозможен извне класса.
-     * Придётся закомментить: ошибка Cannot access 'name': it is private in 'Person'
-     * fun main() {
-     *    val Iam = Person()
-     *    Iam.getName()
-     *    Iam.name = "Anastasiya"
-     *    Iam.getName()}
-     */
-class Person {
-    private var name = "Nastya"
-    private var age = 30
+ */
+class Person(private var name: String, private var age: Int) {
     fun getName() {
         println("Меня зовут $name")
     }
@@ -20,4 +12,12 @@ class Person {
     fun getAge() {
         println("Мне $age лет")
     }
+}
+
+fun main() {
+    val Iam = Person("Anastasiya", 30)
+    Iam.getName()
+    Iam.getAge()
+    var privAge = Iam.age // Cannot access 'age': it is private in 'Person'
+    println(privAge)
 }
