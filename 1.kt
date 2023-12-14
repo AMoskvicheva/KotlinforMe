46f43988d6aa035c35d9ef8e78061869c76b4ca2
AMoskvicheva <149317632+AMoskvicheva@users.noreply.github.com>
2023-12-14 13:40:11 +0600

    Update 1.kt
    чистовик

--- 1.kt
+++ 1.kt
@@ -1,22 +1,17 @@
-package com.example.oo
-
 /**
  * 1. Создайте класс Car с полями model, color, year и методом drive().Создайте несколько объектов
  * этого класса и вызовите их метод drive().
  */
-open class Car(model: String, color: String, year: String) {
-    var m = model
-    var c = color
-    var y = year
+open class Car(var model: String, var color: String, var year: Int) {
     fun drive() {
-        println("Поехала машина $m $c $y")
+        println("Поехала машина $model $color $year")
     }
 }
 
 fun main() {
-    val myCar1 = Car("Priora", "violet", "2000")
-    val myCar2 = Car("Niva", "white", "1987")
-    val myCar3 = Car("Volkswagen", "red", "2023")
+    val myCar1 = Car("Priora", "violet", 2000)
+    val myCar2 = Car("Niva", "white", 1987)
+    val myCar3 = Car("Volkswagen", "red", 2023)
     myCar1.drive()
     myCar2.drive()
     myCar3.drive()
